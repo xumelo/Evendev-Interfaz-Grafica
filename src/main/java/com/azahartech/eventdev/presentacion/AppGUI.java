de35bc84bd22478163620a2026a8bef1b1fe1ce1
alejuadra <alejuadro@alu.edu.gva.es>
2026-02-18 16:16:34 +0100

    Practica U7 Practica1 Completada

--- src/main/java/com/azahartech/eventdev/presentacion/AppGUI.java
+++ src/main/java/com/azahartech/eventdev/presentacion/AppGUI.java
@@ -1,12 +1,13 @@
 package com.azahartech.eventdev.presentacion;
 
 import com.azahartech.eventdev.vista.VistaLogin;
+import com.azahartech.eventdev.vista.VistaRegistro;
 
 import javax.swing.*;
 
 public class AppGUI {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {new VistaLogin().setVisible(true);});
-
+        SwingUtilities.invokeLater(() -> {new VistaRegistro().setVisible(true);});
     }
 }
---
+++ src/main/java/com/azahartech/eventdev/vista/VistaRegistro.java
@@ -0,0 +1,60 @@
+package com.azahartech.eventdev.vista;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class VistaRegistro extends JFrame {
+    private Container lienzo = this.getContentPane();
+    public VistaRegistro(){
+        this.setTitle("Registro");
+        this.setSize(400, 300);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+        lienzo.setLayout(new BorderLayout(10, 10));
+        initUI();
+    }
+    private void initUI(){
+        JPanel pnlFormulario=new JPanel();
+        pnlFormulario.setLayout(new GridLayout(5,5,5,5));
+        JLabel lblNombre = new JLabel("Nombre Completo:");
+        JTextField txtNombre = new JTextField();
+        JLabel lblEmail = new JLabel("Email:");
+        JTextField txtEmail = new JTextField();
+        JLabel lblPassword = new JLabel("Contraseña:");
+        JPasswordField txtPassword = new JPasswordField();
+        JLabel lblPasswordConfirmar = new JLabel("Confirmar Contraseña:");
+        JPasswordField txtPasswordConfirmar = new JPasswordField();
+        JLabel lblEdad = new JLabel("Edad:");
+        JTextField txtEdad = new JTextField();
+        pnlFormulario.add(lblNombre);
+        pnlFormulario.add(txtNombre);
+        pnlFormulario.add(lblEmail);
+        pnlFormulario.add(txtEmail);
+        pnlFormulario.add(lblPassword);
+        pnlFormulario.add(txtPassword);
+        pnlFormulario.add(lblPasswordConfirmar);
+        pnlFormulario.add(txtPasswordConfirmar);
+        pnlFormulario.add(lblEdad);
+        pnlFormulario.add(txtEdad);
+        lienzo.add(pnlFormulario, BorderLayout.CENTER);
+
+        //Botones
+        JPanel pnlBotones =new JPanel();
+        //Alineacion al centro
+        pnlBotones.setLayout(new FlowLayout(FlowLayout.CENTER));
+        //Crear Botones
+        JButton btnGuardar=new JButton("Guardar");
+        JButton btnCancelar=new JButton("Cancelar");
+        pnlBotones.add(btnGuardar);
+        pnlBotones.add(btnCancelar);
+        lienzo.add(pnlBotones,BorderLayout.SOUTH);
+        //Titulo
+        JLabel lblTitulo = new JLabel("Registro a EventDEV");
+        //Cambiar la fuente
+        lblTitulo.setFont(new Font("Arial", Font.BOLD, 18));
+        //Alineación horizontal al centro
+        lblTitulo.setHorizontalAlignment(SwingConstants.CENTER);
+        //Añadir la etiqueta a la zona NORTH
+        lienzo.add(lblTitulo, BorderLayout.NORTH);
+    }
+}
