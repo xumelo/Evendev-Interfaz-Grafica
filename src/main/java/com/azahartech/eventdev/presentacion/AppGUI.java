a3f61bffec33b4f1b336acad53eb3e720992547b
alejuadra <alejuadro@alu.edu.gva.es>
2026-03-04 17:32:27 +0100

    Practica U7 Practica2 Completada

--- src/main/java/com/azahartech/eventdev/presentacion/AppGUI.java
+++ src/main/java/com/azahartech/eventdev/presentacion/AppGUI.java
@@ -11,11 +11,12 @@ public class AppGUI {
         SwingUtilities.invokeLater(() -> {
             new VistaLogin().setVisible(true);
         });
-//        SwingUtilities.invokeLater(() -> {
-//            new VistaRegistro().setVisible(true);
-//        });
-//        SwingUtilities.invokeLater(() -> {
-//            new VistaDashboard("admin@eventdev.com").setVisible(true);
-//        });
+/*
+        SwingUtilities.invokeLater(() -> {
+            new VistaRegistro().setVisible(true);
+        });
+        SwingUtilities.invokeLater(() -> {
+            new VistaDashboard("admin@eventdev.com").setVisible(true);
+        });*/
     }
 }
